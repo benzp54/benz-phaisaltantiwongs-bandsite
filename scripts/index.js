e13e16251c955d2ca2cf83e341933ce4dd449262
benzp <benz.pwongs@live.com>
2022-04-02 20:59:06 -0700

    Updated JavaScript

---
+++ scripts/index.js
@@ -0,0 +1,80 @@
+const commentsLog = [
+    {
+        id: 1,
+        userName: "Miles Acosta",
+        userComment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+        timeStamp: "12/20/2020",
+    },
+    {
+        id: 2,
+        userName: "Emilie Beach",
+        userComment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+        timeStamp: "01/09/2021",
+    },
+    {
+        id: 3,
+        userName = "Conner Walton",
+        userComment = "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+        timeStamp: "02/17/2021"
+    },
+];
+
+//Button handler: Need to prevent page refresh upon click
+function buttonHandler() {
+    const commentsForm = document.getElementById('comments__form');
+    commentsForm.addEventListener('submit', e => {
+    e.preventDefault();
+    });
+    console.log('button clicked');
+    /* addComment ();
+    */
+};
+
+const btn = document.getElementById("btn__comment")
+btn.addEventListener('click', buttonHandler);
+
+
+/*Need to capture form data before reset
+    console.log(e.target.userName.value);
+    console.log(e.target.userComment.value);
+    console.log(e.target.timeStamp.value);
+    console.log("form submitted");
+});
+
+//Add function to append new comment to the commentsLog array
+function buttonHandler(onClick) {
+    const newUser = userName;
+    const newComment = userComment;
+    const submitTime = timeStamp;
+    console.log('Clicked!');
+}
+
+//Adding new comment to the commentsLog array and clear field upon click
+let addComment = () => {    
+    let nameField = document.getElementById('newName')
+    let commentField = document.getElementById('newComment')
+
+    let comment = commentField.value;
+    commentField.Value = "";
+
+    let name = nameField.value;
+    nameField.value = "";
+
+    const commentObject = {
+        userName: name,
+        userComment: comment,
+    };
+
+    commentsLog.unshift(commentObject);
+    console.log('Array updated, form reset')
+    render();
+};
+
+//Adding new comment to render on the web page
+let displayComment = () => {
+    let div = document.createElement("div");
+    let p = document.createElement ("p");
+    comments__list.append(commentsLog, p) //Revisit
+}
+
+//You must have a function called displayComment() that takes in one comment object as a parameter and displays it on the page using JavaScript DOM manipulation.*/
